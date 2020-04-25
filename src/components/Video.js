e3e6579fb4b2555c2831176987ea1cd69e5b0d76
nkosakul <nkosakul@3pc.de>
2020-04-26 00:44:45 +0200

    feat(video): optimize video by lazy-loading it and finish styling

--- src/components/Video.js
+++ src/components/Video.js
@@ -2,36 +2,43 @@ import React, { useRef, useEffect } from 'react';
 import { APILazerLoader } from '../modules/lazer-loader';
 import { getPlyr, Video as Plyr } from '../modules/video/create-plyr';
 
+const createPlayer = async (api, container) => {
+  const pl = new Plyr(api, container, {});
+  const player = await pl.create();
+  return player;
+};
+
+const observe = (targets) => {
+  new APILazerLoader({
+    targets,
+    config: {
+      rootMargin: '50px 0px',
+      threshold: 0.01,
+    },
+    cb: (api, { target }) => createPlayer(api, target),
+    loadAPI: () => getPlyr(),
+  }).init();
+};
+
 const Video = ({ props }) => {
   const videoEl = useRef(null);
 
-  const createPlayer = async (api, container) => {
-    const pl = new Plyr(api, container, {});
-    const player = await pl.create();
-    return player;
-  };
-
   useEffect(() => {
-    const observe = (targets) => {
-      new APILazerLoader({
-        targets,
-        config: {
-          rootMargin: '50px 0px',
-          threshold: 0.01,
-        },
-        cb: (api, { target }) => createPlayer(api, target),
-        loadAPI: () => getPlyr(),
-      }).init();
-    };
-
-    observe([videoEl.current]);
+    if (videoEl && videoEl.current) observe([videoEl.current]);
   }, []);
+
   return (
-    <div
-      ref={videoEl}
-      data-plyr-provider="youtube"
-      data-plyr-embed-id={props.youtubeUrl}
-    ></div>
+    <div className="video-player">
+      <div className="video-player__inner">
+        <div>
+          <div
+            ref={videoEl}
+            data-plyr-provider="youtube"
+            data-plyr-embed-id={props.youtubeUrl}
+          ></div>
+        </div>
+      </div>
+    </div>
   );
 };
 
