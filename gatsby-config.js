9da1014fe5bc10388f552e8217c716388c9c26df
nkosakul <nkosakul@3pc.de>
2020-04-26 14:16:01 +0200

    feat: using loadable serverside rendering

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,7 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
+    'gatsby-plugin-loadable-components-ssr',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
