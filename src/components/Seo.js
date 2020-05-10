80ff549718331caef7a45d1d1a736679cd1ae1d4
nkosakul <nkosakul@3pc.de>
2020-05-10 10:59:51 +0200

    feat(seo): adding some more meta tags like keywords and og:site_name

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -14,13 +14,18 @@ const SEO = ({ pageTitle, pageUrl }) => {
       <meta name="description" content={description} />
       <meta name="generator" content="Gatsby.js"></meta>
       <meta name="robots" content="index, follow"></meta>
+      <meta
+        name="keywords"
+        content="Pepita Bauhardt, Dancer, Choach, Choreographer, Pepita, Pepita Maria Bauhardt, Bauhardt, Berlin, Dance, Videos, Tanz, tanzen"
+      ></meta>
       <meta name="og:title" content={siteTitle} />
       <meta name="og:description" content={description} />
       <meta name="og:type" content="website" />
+      <meta property="og:site_name" content="Pepita Maria Bauhardt"></meta>
       {/* TODO: add correct favicon */}
       {/* TODO: add image for page */}
       <meta property="og:image" content="1200x630"></meta>
-      <meta property="og:locale" content="de"></meta>
+      <meta property="og:locale" content="de_DE"></meta>
       <meta property="og:url" content={url}></meta>
       <meta name="twitter:card" content="summary_large_image"></meta>
       <meta name="twitter:title" content={title}></meta>
